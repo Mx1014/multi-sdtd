939d014ccf22293469b0056a2ff6249c0f97e93a
liuze713 <liuze713@hotmail.com>
2017-11-28 16:52:03 +0800

    配置数据源

--- census-server/src/main/java/com/rzt/census/CensusServerApplication.java
+++ census-server/src/main/java/com/rzt/census/CensusServerApplication.java
@@ -2,11 +2,16 @@ package com.rzt.census;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+/**
+ * Druid监控配置注解
+ */
+@ServletComponentScan
 public class CensusServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CensusServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CensusServerApplication.class, args);
+    }
 }
