4ff746fb46a07491d356009486931cc1330b3c8a
ningweize <907695276@qq.com>
2017-11-28 09:48:36 +0800

    首次提交巡视任务微服务

---
+++ xstask-server/src/main/java/com/rzt/xstask/XstaskServerApplication.java
@@ -0,0 +1,12 @@
+package com.rzt.xstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XstaskServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XstaskServerApplication.class, args);
+	}
+}
---
+++ xstask-server/src/test/java/com/rzt/xstask/XstaskServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rzt.xstask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XstaskServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
