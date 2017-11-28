54521a1dd82e42d0dd5589aa1b2ad681b5346179
liuze713 <liuze713@hotmail.com>
2017-11-28 10:28:20 +0800

    第一次提交统计服务

---
+++ census-server/src/test/java/com/rzt/census/census/CensusServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rzt.census.census;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CensusServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
