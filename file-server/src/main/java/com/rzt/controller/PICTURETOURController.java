3988ad417eab711111028e91ece9798ce61dbc7c
李泽州 <lizezhou1231@outlook.com>
2017-12-08 14:29:28 +0800

    重新写了上传工具类

--- file-server/src/main/java/com/rzt/controller/PICTURETOURController.java
+++ file-server/src/main/java/com/rzt/controller/PICTURETOURController.java
@@ -6,16 +6,18 @@
  */
 package com.rzt.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.rzt.entity.PICTURETOUR;
 import com.rzt.service.PICTURETOURService;
 import com.rzt.utils.DateUtil;
 import com.rzt.utils.ImageUtils;
 import com.rzt.utils.SnowflakeIdWorker;
+import com.rzt.utils.YmlConfigUtil;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,12 +27,16 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 类名称：PICTURETOURController
- * 类描述：    
+ * 类描述：巡视照片控制类
  * 创建人：张虎成   
  * 创建时间：2017/11/29 09:35:42 
  * 修改人：张虎成    
@@ -45,26 +51,116 @@ public class PICTURETOURController extends
 
     protected static Logger LOGGER = LoggerFactory.getLogger(PICTURETOURController.class);
 
-    @Autowired
-    private Environment env;
+    @ApiOperation(
+            value = "文件上传",
+            notes = "上传文件信息及参数"
+    )
+    @PostMapping("fileUpload")
+    public Map<String, Object> fileUpload(MultipartFile file,String jsonStr) {
 
-	@ApiOperation(
-			value = "文件上传",
-			notes = "上传文件信息及参数"
-	)
-	@PostMapping("fileUpload")
-	public synchronized void fileUpload(MultipartFile file,String taskId) {
-	    //判断taskId是否为空
-	    if(taskId== null||"".equals(taskId)){
-	        //return;
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        String taskId = getJsonObjectValue(jsonStr,"taskId");
+
+        Map<String, Object> result = new HashMap<>();
+
+        PICTURETOUR picturetour = new PICTURETOUR();
+        //判断taskId是否为空
+        if(taskId== null||"".equals(taskId)){
+            //return;
             // TODO: 2017/12/1 压力测试用
             taskId = "taskId";
         }
-		//判断文件是否为空,taskId是否为空
-		if(!file.isEmpty()){
-		    //每天存一个文件夹、每个任务类型一个文件夹、每个任务一个文件夹
+
+        try {
+            Map<String, Object> map = ImageUtils.resizeDefault(file, taskId);
+            if("true".equals(map.get("success").toString())){
+
+                String targetPath = map.get("targetPath").toString();
+                String thumPath = map.get("thumPath").toString();
+                String picName = map.get("picName").toString();
+                picturetour.setId("");
+                picturetour.setCreateTime(new Date(System.currentTimeMillis()));
+                picturetour.setFileName(picName);
+                picturetour.setFilePath(targetPath);
+                picturetour.setFileSmallPath(thumPath);
+                picturetour.setFileType("1");
+                picturetour.setTaskId(taskId);
+                picturetour.setUserId(getJsonObjectValue(jsonStr,"userId"));
+                picturetour.setProcessId(getJsonObjectValue(jsonStr,"processId"));
+                picturetour.setProcessName(getJsonObjectValue(jsonStr,"processName"));
+                picturetour.setLat(getJsonObjectValue(jsonStr,"lat"));
+                picturetour.setLon(getJsonObjectValue(jsonStr,"lon"));
+
+                service.add(picturetour);
+                result.put("success",true);
+                result.put("thumPath",thumPath);
+                System.out.println(targetPath);
+                System.out.println(thumPath);
+                System.out.println(picName);
+            }
+
+        } catch (IOException e) {
+            LOGGER.error("上传文件失败！",e);
+            result.put("success",false);
+        }
+
+        return result;
+
+    }
+
+    private String getJsonObjectValue(String jsonStr,String key){
+        if(jsonStr==null||"".equals(jsonStr)){
+            LOGGER.error("jsonStr为空！");
+            return null;
+        }
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        if(jsonObject.containsKey(key)){
+            return jsonObject.getString(key);
+        }else{
+            LOGGER.error("json对象中没有此key！" +
+                    "\n json对象为："+jsonStr+
+                    "\n key="+key);
+        }
+        return null;
+    }
+
+    @ApiOperation(
+            value = "获取巡视任务照片",
+            notes = "根据taskId获取某条任务的所有照片，返回List<PICTURETOUR>"
+    )
+    @GetMapping("getTourImgsBytaskId")
+    public Map<String, Object> getTourImgsBytaskId(String taskId) {
+
+        Map<String, Object> result = new HashMap<>();
+        List<PICTURETOUR> list = service.findBytaskId(taskId);
+        result.put("success",true);
+        result.put("object",list);
+
+        return result;
+
+    }
+
+    @ApiOperation(
+            value = "文件上传压力测试",
+            notes = "上传文件信息及参数"
+    )
+    @PostMapping("fileUpload1")
+    public Map<String, Object> fileUpload1(MultipartFile file, String taskId) {
+
+        Map<String, Object> result = new HashMap<>();
+
+        PICTURETOUR picturetour = new PICTURETOUR();
+        //判断taskId是否为空
+        if(taskId== null||"".equals(taskId)){
+            //return;
+            // TODO: 2017/12/1 压力测试用
+            taskId = "taskId";
+        }
+        //判断文件是否为空,taskId是否为空
+        if(!file.isEmpty()){
+            //每天存一个文件夹、每个任务类型一个文件夹、每个任务一个文件夹
             String currentDate = DateUtil.getCurrentDate();
-            String targetDir = env.getProperty("file.upload-dir") + currentDate + File.separator + taskId + File.separator;
+            String targetDir = YmlConfigUtil.getConfigByKey("upload-dir") + currentDate + File.separator + taskId + File.separator;
             File dir = new File(targetDir);
             //目标目录是否存在
             if(!dir.exists()&&!dir.isDirectory()){
@@ -76,18 +172,34 @@ public class PICTURETOURController extends
                 String suffix = originalFilename.substring(originalFilename.lastIndexOf(".")+1,originalFilename.length());
                 //判断下文件是否为图片
                 if("jpg".equals(suffix)){
-                    int picHeight = Integer.parseInt(env.getProperty("file.thum.height"));
-                    int picWidth = Integer.parseInt(env.getProperty("file.thum.width"));
+                    int picHeight = Integer.parseInt(YmlConfigUtil.getConfigByKey("thum-height"));
+                    int picWidth = Integer.parseInt(YmlConfigUtil.getConfigByKey("thum-width"));
                     ImageUtils.resize(file,targetDir + picHeight + "x" + picWidth + originalFilename,picHeight,picWidth);
                 }
                 file.transferTo(targetFile);
+                picturetour.setId("");
+                picturetour.setFileName("");
+                picturetour.setCreateTime(new Date(new java.util.Date().getTime()));
+                result.put("success",true);
             } catch (IOException e) {
+                result.put("success",false);
                 LOGGER.error("文件上传出错，请确保路径设置正确，且拥有读写权限！",e);
             }
-
         }
+        return result;
 
-	}
+    }
+
+    @Scheduled(cron = "0/2 * * * * *")
+    public void timerTask(){
+        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
+        String dirStr = YmlConfigUtil.getConfigByKey("upload-dir");
+        System.out.println(dirStr);
+        File dir = new File(dirStr);
+        File[] files = dir.listFiles();
+
+
+    }
 
     @ApiOperation(
             value = "SnowFlake测试",
--- file-server/src/main/java/com/rzt/utils/ImageUtils.java
+++ file-server/src/main/java/com/rzt/utils/ImageUtils.java
@@ -7,9 +7,17 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageUtils {
+
+    private static String uploadDir = null;
+    private static String thumHeight = null;
+    private static String thumWidth = null;
 
     /**
      * 图片缩放
@@ -48,118 +56,89 @@ public class ImageUtils {
     }
 
     /**
-     * 图片缩放
-     * @param org    原图路径
-     * @param dest   缩放图路径
-     * @param height 高度
-     * @param width  宽度
+     * 保存原图并缩略保存缩略图
+     * @param file    原图文件
+     * @param targetDir   图片存放文件夹名称(原图路径：配置路径+日期+targetDir，缩略图在targetDir下的thum文件夹里)
      */
-    public static boolean resize(String org,String dest, int height, int width) {
-        boolean bol = false; //是否进行了压缩
-        //LogRecord.recode(ImageUtils.class, "resize", org);
-        String pictype="";
-        if(!"".equals(org)&&org!=null){
-            pictype = org.substring(org.lastIndexOf(".")+1,org.length());
-        }
-        //LogRecord.recode(ImageUtils.class, "height:",""+height);
-        // LogRecord.recode(ImageUtils.class, "width:",""+width);
-        double ratio = 0; //缩放比例
-        File o = new File(org);
-        File d = new File(dest);
-        BufferedImage bi;
-        try {
-            bi = ImageIO.read(o);
-            Image itemp = bi.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
-            int itempWidth = bi.getWidth();
-            int itempHeight = bi.getHeight();
-            //LogRecord.recode(ImageUtils.class, "itempHeight:",""+itempHeight);
-            //  LogRecord.recode(ImageUtils.class, "itempWidth:",""+itempWidth);
+    public static Map<String, Object> resizeDefault(MultipartFile file, String targetDir) throws IOException {
 
-            //计算比例
-            if ((itempHeight > height) || (itempWidth > width)) {
-                //LogRecord.recode(ImageUtils.class, "【开始图片缩放】-----pictype:", pictype);
-                ratio = Math.min((new Integer(height)).doubleValue() / itempHeight, (new Integer(width)).doubleValue() / itempWidth);
-                AffineTransformOp op = new AffineTransformOp(AffineTransform.getScaleInstance(ratio, ratio), null);
-                itemp = op.filter(bi, null);
-                ImageIO.write((BufferedImage) itemp,pictype, d);
-                bol = true;
-            }
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        return bol;
-    }
+        Map<String, Object> result = new HashMap<>();
+        String fileName = file.getOriginalFilename();
 
-    public static void resizeWidth(String org, String dest, int height, int width) {
-        //LogRecord.recode(ImageUtils.class, "resizeWidth", org);
-        String pictype="";
-        if(!"".equals(org)&&org!=null){
-            pictype = org.substring(org.lastIndexOf(".")+1,org.length());
+        if(file.isEmpty()){
+            throw new IOException("上传文件为空！");
+        }
+        String picType="";
+        String picName="";
+        picType = fileName.substring(fileName.lastIndexOf(".")+1,fileName.length());
+        picName = fileName.substring(0,fileName.lastIndexOf("."));
+        if(!"jpg".equals(picType)){
+            throw new IOException("上传文件不是jpg图片！");
         }
         double ratio = 0; //缩放比例
-        File o = new File(org);
-        File d = new File(dest);
+        InputStream o = file.getInputStream();
+        String currentDate = DateUtil.getCurrentDate();
+        String baseDir = uploadDir + currentDate + File.separator + targetDir + File.separator;
+        String targetPath = baseDir+picName+"."+picType;
+        String thumPath = baseDir+"thum"+File.separator+picName+thumHeight+"x"+thumWidth+"."+picType;
+
+        //原文件存放路径
+        File targetFile = new File(targetPath);
+        //缩略图存放路径
+        File thumFile = new File(thumPath);
+        //目标目录是否存在
+        File folder = new File(baseDir + "thum");
+        if(!folder.exists()&&!folder.isDirectory()){
+            folder.mkdirs();
+        }
+
+        int width = Integer.parseInt(thumWidth);
+        int height = Integer.parseInt(thumHeight);
         BufferedImage bi;
-        try {
-            bi = ImageIO.read(o);
-            Image itemp = bi.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
-            int itempWidth = bi.getWidth();
-            // LogRecord.recode(ImageUtils.class, "itempWidth:",""+itempWidth);
+        bi = ImageIO.read(o);
+        Image itemp = bi.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
+        int itempWidth = bi.getWidth();
+        int itempHeight = bi.getHeight();
 
-            //计算比例
-            if (itempWidth != width) {
-                //LogRecord.recode(ImageUtils.class, "【开始图片缩放 width】-----pictype:", pictype);
-                ratio = ((new Integer(width)).doubleValue() / itempWidth);
-                AffineTransformOp op = new AffineTransformOp(AffineTransform.getScaleInstance(ratio, ratio), null);
-                itemp = op.filter(bi, null);
-                ImageIO.write((BufferedImage) itemp,pictype, d);
-            }
-
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+        //计算比例
+        if ((itempHeight > height) || (itempWidth > width)) {
+            ratio = Math.min((new Integer(height)).doubleValue() / itempHeight, (new Integer(width)).doubleValue() / itempWidth);
+            AffineTransformOp op = new AffineTransformOp(AffineTransform.getScaleInstance(ratio, ratio), null);
+            itemp = op.filter(bi, null);
+            ImageIO.write((BufferedImage) itemp,picType, thumFile);
         }
+
+        //保存原图到指定路径
+        file.transferTo(targetFile);
+        result.put("picName",picName+"."+picType);
+        result.put("targetPath",targetPath);
+        result.put("thumPath",thumPath);
+        result.put("success",true);
+
+        return result;
+
     }
 
-    public static void copyFile(String sourcePath, String targetPath){
-        BufferedInputStream inBuff=null;
-        BufferedOutputStream outBuff=null;
-        try {
-            File sourceFile = new File(sourcePath);
-            File targetFile = new File(targetPath);
-            if(!targetFile.exists()){
-                targetFile.getParentFile().mkdirs();
-            }
-            // 新建文件输入流并对它进行缓冲
-            inBuff=new BufferedInputStream(new FileInputStream(sourceFile));
+    //设定配置参数
+    static{
 
-            // 新建文件输出流并对它进行缓冲
-            outBuff=new BufferedOutputStream(new FileOutputStream(targetFile));
-
-            // 缓冲数组
-            byte[] b=new byte[1024 * 5];
-            int len;
-            while((len=inBuff.read(b)) != -1) {
-                outBuff.write(b, 0, len);
-            }
-            // 刷新此缓冲的输出流
-            outBuff.flush();
-        } catch(IOException e){
-            //LogRecord.recode(ImageUtils.class, "图片复制异常:", e.getMessage());
-        } finally {
-            // 关闭流
-            try {
-                if(inBuff != null)
-                    inBuff.close();
-                if(outBuff != null)
-                    outBuff.close();
-            } catch (IOException e) {
-                //LogRecord.recode(ImageUtils.class, "文件流关闭异常:", e.getMessage());
-            }
-
+        System.out.println(uploadDir);
+        System.out.println(thumHeight);
+        System.out.println(thumWidth);
+        if(uploadDir==null){
+            uploadDir = YmlConfigUtil.getConfigByKey("upload-dir");
         }
+        if(thumHeight==null){
+            thumHeight = YmlConfigUtil.getConfigByKey("thum-height");
+        }
+        if(thumWidth==null){
+            thumWidth = YmlConfigUtil.getConfigByKey("thum-width");
+        }
+        System.out.println(uploadDir);
+        System.out.println(thumHeight);
+        System.out.println(thumWidth);
     }
+
 
     public static void main(String[] args) throws IOException{
         //pressImage("D:\\images\\444.jpg", "D:\\images\\wmlogo.gif", 100, 50, 0.5f);
