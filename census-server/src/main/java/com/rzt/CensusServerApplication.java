fa9e0b8064246b8c582e2b5e9edb8c425f4f1784
liuze713 <liuze713@hotmail.com>
2017-12-11 17:17:00 +0800

    app流程

--- census-server/src/main/java/com/rzt/CensusServerApplication.java
+++ census-server/src/main/java/com/rzt/CensusServerApplication.java
@@ -1,6 +1,7 @@
 package com.rzt;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.rzt.websocket.serverendpoint.AlarmSituationServerEndpoint;
 import com.rzt.websocket.serverendpoint.PersonnelTasksServerEndpoint;
@@ -9,10 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 统计服务
+ */
 @SpringBootApplication
 @ServletComponentScan
 @EnableScheduling
@@ -52,7 +60,15 @@ public class CensusServerApplication {
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         //创建封装对象
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        converter.setFeatures(SerializerFeature.WriteDateUseDateFormat, SerializerFeature.WriteMapNullValue);
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteDateUseDateFormat);
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
+
+        //处理中文乱码问题(不然出现中文乱码)
+        List<MediaType> fastMediaTypes = new ArrayList<MediaType>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        converter.setSupportedMediaTypes(fastMediaTypes);
+        converter.setFastJsonConfig(fastJsonConfig);
         return new HttpMessageConverters(converter);
     }
 }
