4c7169c4a1d0869a36caf61b23aa67c8b4cdd8f0
李泽州 <lizezhou1231@outlook.com>
2017-12-11 11:32:16 +0800

    工具类

--- linedata-server/src/main/java/com/rzt/controller/CMLINEController.java
+++ linedata-server/src/main/java/com/rzt/controller/CMLINEController.java
@@ -9,9 +9,14 @@ package com.rzt.controller;
 import com.rzt.entity.CMLINE;
 import com.rzt.service.CMLINEService;
 import com.rzt.util.WebApiResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 类名称：CMLINEController
@@ -28,9 +33,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CMLINEController extends
 		CurdController<CMLINE,CMLINEService> {
 
-	@GetMapping("test")
-    public WebApiResponse test(){
-    		return WebApiResponse.success(service.test());
+	@ApiOperation(value = "获取线路名称列表",notes = "获取线路名称列表")
+	@GetMapping("getLineInfo")
+	public WebApiResponse getLineInfo(String kv){
+		ArrayList<String> params = new ArrayList<>();
+		String sql = "select * from cm_line where is_del=0 " ;
+		if(kv!=null&&!"".equals(kv)){
+			params.add(kv);
+			sql += " and v_level = ?" + params.size();
+		}
+		sql += " ORDER BY NLSSORT(line_name,'NLS_SORT = SCHINESE_PINYIN_M')";
+		List<Map<String, Object>> maps = service.execSql(sql,params.toArray());
+		return WebApiResponse.success(maps);
 	}
 	
 }
--- linedata-server/src/main/java/com/rzt/controller/CMLINETOWERController.java
+++ linedata-server/src/main/java/com/rzt/controller/CMLINETOWERController.java
@@ -7,11 +7,21 @@
 package com.rzt.controller;
 import com.rzt.entity.CMLINETOWER;
 import com.rzt.service.CMLINETOWERService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.rzt.util.WebApiResponse;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.rzt.controller.CurdController;
-/**      
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
  * 类名称：CMLINETOWERController
  * 类描述：    
  * 创建人：张虎成   
@@ -25,7 +35,29 @@ import com.rzt.controller.CurdController;
 @RequestMapping("CMLINETOWER")
 public class CMLINETOWERController extends
 		CurdController<CMLINETOWER,CMLINETOWERService> {
-    
-    
-	
+
+	@ApiOperation(value = "杆塔坐标维护接口",notes = "获取杆塔坐标，分页查询")
+	@GetMapping("getLineTowerPosition")
+	public WebApiResponse getLineTowerPosition(@RequestParam(value = "page",defaultValue = "0") Integer page, @RequestParam(value = "size",defaultValue = "15") Integer size, String tdOrg, String kv, String lineId) {
+		Pageable pageable = new PageRequest(page, size);
+		List<String> list = new ArrayList<>();
+		Object[] objects = list.toArray();
+		String sql = "select l.v_level,l.line_name,l.section,LT.tower_name,t.longitude,t.latitude from cm_line l,cm_line_tower lt,cm_tower t " +
+				"where l.id=LT.line_id and LT.tower_id=t.id";
+		if(tdOrg!=null&&!"".equals(tdOrg.trim())){
+			list.add(tdOrg);
+			sql += " and l.td_org= ?" + list.size();
+		}
+		if(kv!=null&&!"".equals(kv.trim())){
+			list.add(kv);
+			sql += " and v_level= ?" + list.size();
+		}
+		if(lineId!=null&&!"".equals(lineId.trim())){
+			list.add(lineId);
+			sql += " and line_id= ?" + list.size();
+		}
+		sql += " order by lt.sort";
+		Page<Map<String, Object>> maps = service.execSqlPage(pageable, sql,list.toArray());
+		return WebApiResponse.success(maps);
+	}
 }
