01d98a1efe7095ed14e7cc3ea5aac47b3b133ae7
liuze713 <liuze713@hotmail.com>
2017-12-12 13:31:15 +0800

    app流程

--- userCenter/src/main/java/com/rzt/controller/RztSysUserController.java
+++ userCenter/src/main/java/com/rzt/controller/RztSysUserController.java
@@ -1,8 +1,8 @@
-/**    
+/**
  * 文件名：RztSysUserController
- * 版本信息：    
- * 日期：2017/10/10 17:28:27    
- * Copyright 融智通科技(北京)股份有限公司 版权所有    
+ * 版本信息：
+ * 日期：2017/10/10 17:28:27
+ * Copyright 融智通科技(北京)股份有限公司 版权所有
  */
 package com.rzt.controller;
 
@@ -12,6 +12,7 @@ import com.rzt.entity.RztSysUserauth;
 import com.rzt.service.RztSysUserService;
 import com.rzt.service.RztSysUserauthService;
 import com.rzt.util.WebApiResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -31,164 +32,179 @@ import java.util.Map;
 
 /**
  * 类名称：RztSysUserController
- * 类描述：    
- * 创建人：张虎成   
- * 创建时间：2017/10/10 17:28:27 
- * 修改人：张虎成    
- * 修改时间：2017/10/10 17:28:27    
- * 修改备注：    
- * @version        
+ * 类描述：
+ * 创建人：张虎成
+ * 创建时间：2017/10/10 17:28:27
+ * 修改人：张虎成
+ * 修改时间：2017/10/10 17:28:27
+ * 修改备注：
  */
 @RestController
 @RequestMapping("RztSysUser")
 public class RztSysUserController extends
-		CurdController<RztSysUser,RztSysUserService> {
-	@Autowired
-	private RztSysUserauthService userauthService;
+        CurdController<RztSysUser, RztSysUserService> {
+    @Autowired
+    private RztSysUserauthService userauthService;
 
-	@PostMapping("addUser/{password}")
-	public WebApiResponse addUser(@RequestParam("file") MultipartFile file, @PathVariable String password, @ModelAttribute RztSysUser user) {
-		String filePath = "";
-		if (!file.isEmpty()) {
-			// 获取文件名
-			String fileName = file.getOriginalFilename();
-			// 文件上传后的路径
-			filePath = "E://test//" + fileName;
-			File dest = new File(filePath);
-			try {
-				file.transferTo(dest);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		user.setAvatar(filePath);
-		String flag = userauthService.findByUserName(user);
-		if (!flag.equals("1")){
-			return WebApiResponse.erro(flag);
-		}
-		user.setCreatetime(new Date());
-		this.service.add(user);
-		userauthService.addUserAuth(user,password);
-		return WebApiResponse.success("添加成功！");
-	}
+    @PostMapping("addUser/{password}")
+    public WebApiResponse addUser(@RequestParam("file") MultipartFile file, @PathVariable String password, @ModelAttribute RztSysUser user) {
+        String filePath = "";
+        if (!file.isEmpty()) {
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            // 文件上传后的路径
+            filePath = "E://test//" + fileName;
+            File dest = new File(filePath);
+            try {
+                file.transferTo(dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        user.setAvatar(filePath);
+        String flag = userauthService.findByUserName(user);
+        if (!flag.equals("1")) {
+            return WebApiResponse.erro(flag);
+        }
+        user.setCreatetime(new Date());
+        this.service.add(user);
+        userauthService.addUserAuth(user, password);
+        return WebApiResponse.success("添加成功！");
+    }
 
-	@DeleteMapping("deleteUser/{id}")
-	public WebApiResponse deleteUser(@PathVariable String id){
-		this.service.delete(id);
-		userauthService.deleteAuthByUserId(id);
-		return WebApiResponse.success("删除成功！");
-	}
+    @DeleteMapping("deleteUser/{id}")
+    public WebApiResponse deleteUser(@PathVariable String id) {
+        this.service.delete(id);
+        userauthService.deleteAuthByUserId(id);
+        return WebApiResponse.success("删除成功！");
+    }
 
-	@DeleteMapping("deleteBatchUser")
-	public WebApiResponse deleteBatchUser(@RequestParam String ids){
-		try {
-			this.service.deleteSome(ids);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return WebApiResponse.success("批量删除失败！");
-		}
-		userauthService.deleteBatchAuthByUserId(ids);
-		return WebApiResponse.success("批量删除成功！");
-	}
-
-
-	@PatchMapping("updateUser/{id}/{password}")
-	public WebApiResponse updateUser(@PathVariable String id,@ModelAttribute RztSysUser user,
-											 @PathVariable String password){
-		try {
-			this.service.update(user,id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return WebApiResponse.success("更新失败！");
-		}
-		userauthService.updateUserAuth(id,password);
-		return WebApiResponse.success("更新成功!");
-	}
-
-	@GetMapping("findAllUser/{page}/{size}")
-	public List<Map<String,Object>> findAllUser(@PathVariable int page, @PathVariable int size, @RequestParam(required=false)
-		String name){
-		List<Map<String,Object>> pageList = this.service.findUserList(page,size);
-		return pageList;
-	}
-
-	@GetMapping("findUserById/{id}")
-	@ResponseBody
-	public RztSysUser findAllUser(@PathVariable String id){
-		RztSysUser user = this.service.findOne(id);
-		return user;
-	}
-
-	/**
-	 *
-	 * @param flag 0 账号登录 1 手机号登录  2 邮箱登录
-	 * @param loginType 0 APP登录 1 PC登录
-		 * @param account 账号
-	 * @param password 密码
-	 * @param request
-	 * @return
-	 */
-	@GetMapping("login/{flag}/{loginType}")
-	public WebApiResponse login(@PathVariable int flag,@PathVariable int loginType,@RequestParam String account,
-										@RequestParam String password, HttpServletRequest request){
-
-		RztSysUserauth userauth = userauthService.findByIdentifierAndIdentityTypeAndPassword(flag,account,password);
-		RztSysUser sysUser = null;
-		if (userauth != null){
-			sysUser = this.service.findOne(userauth.getUserid());
-			sysUser.setLoginstatus(1);
-			try {
-				this.service.update(sysUser,sysUser.getId());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			userauthService.updateLoginIp(request.getRemoteAddr(),sysUser.getId(),flag);
-		}
-		else{
-			return WebApiResponse.erro("用户名或密码不正确！");
-		}
-		request.getSession().setAttribute("user",sysUser);
-
-		return WebApiResponse.success(sysUser);
-	}
-
-	@GetMapping("login/{flag}")
-	public WebApiResponse login(@PathVariable int flag,@RequestParam String account,
-											@RequestParam String password,@RequestParam String deptid){
-		RztSysUser sysUser = this.service.findByUsernameAndDeptid(account,deptid);
+    @DeleteMapping("deleteBatchUser")
+    public WebApiResponse deleteBatchUser(@RequestParam String ids) {
+        try {
+            this.service.deleteSome(ids);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.success("批量删除失败！");
+        }
+        userauthService.deleteBatchAuthByUserId(ids);
+        return WebApiResponse.success("批量删除成功！");
+    }
 
 
-		if (sysUser != null){
-			RztSysUserauth userauth = userauthService.findByIdentifierAndIdentityTypeAndPassword(flag,account,password);
-			if (userauth != null)
-				sysUser.setLoginstatus(1);
-			else
-				return WebApiResponse.erro("用户名或密码不正确！");
-			try {
-				this.service.update(sysUser,sysUser.getId());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		else{
-			return WebApiResponse.erro("用户名或单位不正确！");
-		}
+    @PatchMapping("updateUser/{id}/{password}")
+    public WebApiResponse updateUser(@PathVariable String id, @ModelAttribute RztSysUser user,
+                                     @PathVariable String password) {
+        try {
+            this.service.update(user, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.success("更新失败！");
+        }
+        userauthService.updateUserAuth(id, password);
+        return WebApiResponse.success("更新成功!");
+    }
 
-		return WebApiResponse.success(sysUser);
-	}
+    /**
+     * @GetMapping("findAllUser/{page}/{size}") 保存以前的 12-12 2017
+     * public List<Map<String,Object>> findAllUser(@PathVariable int page, @PathVariable int size, @RequestParam(required=false)
+     * String name){
+     * List<Map<String,Object>> pageList = this.service.findUserList(page,size);
+     * return pageList;
+     * }
+     */
+    /**
+     * 人员列表分页查询
+     *
+     * @param page 页数
+     * @param size 每页行数
+     * @return
+     */
+    @GetMapping("findAllUser")
+    @ApiOperation(value = "人员分页查询", notes = "人员分页查询")
+    public WebApiResponse findAllUser(int page, int size) {
+        try {
+            return WebApiResponse.success(this.service.findUserList(page, size));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("数据错误");
+        }
+    }
 
-	@GetMapping("logOut")
-	public WebApiResponse logOut(HttpServletRequest request){
-		RztSysUser user = (RztSysUser) request.getSession().getAttribute("user");
-		user.setLoginstatus(0);
-		try {
-			this.service.update(user,user.getId());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		request.getSession().removeAttribute("user");
+    @GetMapping("findUserById/{id}")
+    @ResponseBody
+    public RztSysUser findAllUser(@PathVariable String id) {
+        RztSysUser user = this.service.findOne(id);
+        return user;
+    }
 
-		return WebApiResponse.success("退出成功！");
-	}
+    /**
+     * @param flag      0 账号登录 1 手机号登录  2 邮箱登录
+     * @param loginType 0 APP登录 1 PC登录
+     * @param account   账号
+     * @param password  密码
+     * @param request
+     * @return
+     */
+    @GetMapping("login/{flag}/{loginType}")
+    public WebApiResponse login(@PathVariable int flag, @PathVariable int loginType, @RequestParam String account,
+                                @RequestParam String password, HttpServletRequest request) {
+
+        RztSysUserauth userauth = userauthService.findByIdentifierAndIdentityTypeAndPassword(flag, account, password);
+        RztSysUser sysUser = null;
+        if (userauth != null) {
+            sysUser = this.service.findOne(userauth.getUserid());
+            sysUser.setLoginstatus(1);
+            try {
+                this.service.update(sysUser, sysUser.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            userauthService.updateLoginIp(request.getRemoteAddr(), sysUser.getId(), flag);
+        } else {
+            return WebApiResponse.erro("用户名或密码不正确！");
+        }
+        request.getSession().setAttribute("user", sysUser);
+
+        return WebApiResponse.success(sysUser);
+    }
+
+    @GetMapping("login/{flag}")
+    public WebApiResponse login(@PathVariable int flag, @RequestParam String account,
+                                @RequestParam String password, @RequestParam String deptid) {
+        RztSysUser sysUser = this.service.findByUsernameAndDeptid(account, deptid);
+
+
+        if (sysUser != null) {
+            RztSysUserauth userauth = userauthService.findByIdentifierAndIdentityTypeAndPassword(flag, account, password);
+            if (userauth != null)
+                sysUser.setLoginstatus(1);
+            else
+                return WebApiResponse.erro("用户名或密码不正确！");
+            try {
+                this.service.update(sysUser, sysUser.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            return WebApiResponse.erro("用户名或单位不正确！");
+        }
+
+        return WebApiResponse.success(sysUser);
+    }
+
+    @GetMapping("logOut")
+    public WebApiResponse logOut(HttpServletRequest request) {
+        RztSysUser user = (RztSysUser) request.getSession().getAttribute("user");
+        user.setLoginstatus(0);
+        try {
+            this.service.update(user, user.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        request.getSession().removeAttribute("user");
+
+        return WebApiResponse.success("退出成功！");
+    }
 
 }
--- userCenter/src/main/java/com/rzt/service/RztSysUserService.java
+++ userCenter/src/main/java/com/rzt/service/RztSysUserService.java
@@ -1,8 +1,8 @@
-/**    
- * 文件名：RztSysUserService           
- * 版本信息：    
- * 日期：2017/10/10 17:28:27    
- * Copyright 融智通科技(北京)股份有限公司 版权所有    
+/**
+ * 文件名：RztSysUserService
+ * 版本信息：
+ * 日期：2017/10/10 17:28:27
+ * Copyright 融智通科技(北京)股份有限公司 版权所有
  */
 package com.rzt.service;
 
@@ -12,6 +12,7 @@ import com.rzt.repository.RztSysUserRepository;
 import com.rzt.utils.DbUtil;
 import com.rzt.utils.PageUtil;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,42 +23,62 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Map;
 
-/**      
- * 类名称：RztSysUserService    
- * 类描述：InnoDB free: 537600 kB    
- * 创建人：张虎成   
- * 创建时间：2017/10/10 17:28:27 
- * 修改人：张虎成    
- * 修改时间：2017/10/10 17:28:27    
- * 修改备注：    
- * @version        
+/**
+ * 类名称：RztSysUserService
+ * 类描述：InnoDB free: 537600 kB
+ * 创建人：张虎成
+ * 创建时间：2017/10/10 17:28:27
+ * 修改人：张虎成
+ * 修改时间：2017/10/10 17:28:27
+ * 修改备注：
  */
 @Service
 @Transactional
-public class RztSysUserService extends CurdService<RztSysUser,RztSysUserRepository> {
-	@PersistenceContext
-	private EntityManager entityManager;
+public class RztSysUserService extends CurdService<RztSysUser, RztSysUserRepository> {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	public Page<RztSysUser> findByName(String name, Pageable pageable){
-		if (StringUtils.isEmpty(name))
-			return this.reposiotry.findAll(pageable);
-		else{
-			name = "%" + name + "%";
-			return this.reposiotry.findByRealnameLike(name,pageable);
-		}
-	}
+    public Page<RztSysUser> findByName(String name, Pageable pageable) {
+        if (StringUtils.isEmpty(name))
+            return this.reposiotry.findAll(pageable);
+        else {
+            name = "%" + name + "%";
+            return this.reposiotry.findByRealnameLike(name, pageable);
+        }
+    }
 
-	public List<Map<String,Object>> findUserList(int page,int size){
-		StringBuilder builder = new StringBuilder();
-		builder.append("SELECT GROUP_CONCAT(r.id) roleid,GROUP_CONCAT(r.roleName) roleName,u.* from " +
-				"rztsysuser u LEFT JOIN rztsysuserrole l ON u.id = " +
-				"l.userId LEFT JOIN rztsysrole r on l.roleId = r.id GROUP BY u.id");
-		builder.append(PageUtil.getLimit(page,size));
-		return DbUtil.list(entityManager,builder.toString());
-	}
+    public Page<Map<String, Object>> findUserList(int page, int size) {
+//		StringBuilder builder = new StringBuilder();
+//		builder.append("SELECT GROUP_CONCAT(r.id) roleid,GROUP_CONCAT(r.roleName) roleName,u.* from " +
+//				"rztsysuser u LEFT JOIN rztsysuserrole l ON u.id = " +
+//				"l.userId LEFT JOIN rztsysrole r on l.roleId = r.id GROUP BY u.id");
+//		builder.append(PageUtil.getLimit(page,size));
+//		return DbUtil.list(entityManager,builder.toString());
+        Pageable pageable = new PageRequest(page, size);
+        String sql = "SELECT " +
+                "  wm_concat(r.ID) AS roleid," +
+                "  wm_concat(R.roleName) AS roleName," +
+                "  U.REALNAME, " +
+                "  U.USERNAME, " +
+                "  U.EMAIL, " +
+                "  U.PHONE, " +
+                "  U.DEPTID, " +
+                "  U.CLASSNAME, " +
+                "  U.CERTIFICATE, " +
+                "  U.WORKYEAR, " +
+                "  U.WORKTYPE, " +
+                "  U.SERIALNUMBER, " +
+                "  U.AGE, " +
+                "  U.USERTYPE, " +
+                "  U.AVATAR AS file " +
+                "FROM rztsysuser u LEFT JOIN rztsysuserrole l ON u.id = l.userId " +
+                "  LEFT JOIN rztsysrole r ON l.roleId = r.id " +
+                "GROUP BY u.REALNAME, U.REALNAME,U.USERNAME,U.EMAIL,U.PHONE,U.DEPTID,U.CLASSNAME,U.CERTIFICATE,U.WORKYEAR,U.WORKTYPE,U.SERIALNUMBER,U.AGE,U.USERTYPE,U.AVATAR";
+        return this.execSqlPage(pageable, sql);
+    }
 
-	public RztSysUser findByUsernameAndDeptid(String username,String deptid){
-		return this.reposiotry.findByUsernameAndDeptid(username,deptid);
-	}
+    public RztSysUser findByUsernameAndDeptid(String username, String deptid) {
+        return this.reposiotry.findByUsernameAndDeptid(username, deptid);
+    }
 
 }
