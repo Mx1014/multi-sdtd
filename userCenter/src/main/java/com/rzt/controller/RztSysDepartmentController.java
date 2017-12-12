44bc4d08fbb3103a17db0c9a359209cca06728ae
ningweize <907695276@qq.com>
2017-12-12 10:11:44 +0800

    nwz上次提交回退

--- userCenter/src/main/java/com/rzt/controller/RztSysDepartmentController.java
+++ userCenter/src/main/java/com/rzt/controller/RztSysDepartmentController.java
@@ -9,37 +9,32 @@ package com.rzt.controller;
 import com.rzt.controller.CurdController;
 import com.rzt.entity.RztSysDepartment;
 import com.rzt.service.RztSysDepartmentService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-/**      
+/**
  * 类名称：RztSysDepartmentController
- * 类描述：    
- * 创建人：张虎成   
- * 创建时间：2017/10/10 10:26:33 
- * 修改人：张虎成    
- * 修改时间：2017/10/10 10:26:33    
- * 修改备注：    
+ * 类描述：
+ * 创建人：张虎成
+ * 创建时间：2017/10/10 10:26:33
+ * 修改人：张虎成
+ * 修改时间：2017/10/10 10:26:33
+ * 修改备注：
  * @version
  * 部门表
  */
 @RestController
 @RequestMapping("RztSysDepartment")
-@Api(value = "ningweize")
 public class RztSysDepartmentController extends
 		CurdController<RztSysDepartment,RztSysDepartmentService> {
 
 	//新增子节点
 	@ResponseBody
 	@RequestMapping(value = "addSonNode",method = RequestMethod.POST)
-	@ApiOperation(value = "新增子节点",notes = "新增子节点")
-	public RztSysDepartment addSonNode(@RequestParam(required = false) String nodeId, @ModelAttribute RztSysDepartment rztSysDepartment){
-		String id = nodeId;
+	public RztSysDepartment addSonNode(@RequestParam(required = false) String id, @ModelAttribute RztSysDepartment rztSysDepartment){
 		if (StringUtils.isEmpty(id))
 			id = this.service.getRootId();
 		return this.service.addSonNode(id,rztSysDepartment);
@@ -55,7 +50,6 @@ public class RztSysDepartmentController extends
 	//新增同级节点
 	@ResponseBody
 	@RequestMapping(value = "addNode/{id}",method = RequestMethod.POST)
-	@ApiOperation(value = "新增同级节点",notes = "新增同级节点")
 	public RztSysDepartment addNode(@PathVariable String id, @ModelAttribute RztSysDepartment rztSysDepartment){
 		return this.service.addNode(id,rztSysDepartment);
 	}
@@ -69,7 +63,6 @@ public class RztSysDepartmentController extends
 	//根据父节点的id查询子孙节点
 	@RequestMapping(value = "findDeptListByPid",method = RequestMethod.GET)
 	@ResponseBody
-	@ApiOperation(value = "根据父节点的id查询子孙节点",notes = "根据父节点的id查询子孙节点")
 	public List<Map<String, Object>> findDeptListByPid(@RequestParam(required = false) String id){
 		if (StringUtils.isEmpty(id))
 			id = this.service.getRootId();
@@ -79,7 +72,6 @@ public class RztSysDepartmentController extends
 	//根据父节点id查询所有子节点
 	@RequestMapping(value = "findByDeptPid/{id}",method = RequestMethod.GET)
 	@ResponseBody
-	@ApiOperation(value = "根据父节点id查询所有子节点",notes = "根据父节点id查询所有子节点")
 	public List<RztSysDepartment> findByDeptPid(@PathVariable("id") String menuPid){
 		return this.service.findByDeptPid(menuPid);
 	}
