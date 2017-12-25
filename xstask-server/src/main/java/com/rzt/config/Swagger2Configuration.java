0ab656716ac4f5c51d92d58b0ced385cc27e0494
宁伟泽 <907695276@qq.com>
2017-12-25 09:21:31 +0800

    redis配置信息完善  增加连接池的其他属性

---
+++ xstask-server/src/main/java/com/rzt/config/Swagger2Configuration.java
@@ -0,0 +1,39 @@
+package com.rzt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by flysheep on 2017/9/30.
+ */
+//@Configuration
+//@EnableSwagger2
+public class Swagger2Configuration {
+
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rzt.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf() {
+        return new ApiInfoBuilder()
+                .title("security-login-check 接口文档")
+                .description("security-login-check 提供的接口描述文档")
+                .build();
+
+    }
+
+}
