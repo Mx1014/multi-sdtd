98dbb6a4bcfa7c6c31ca0501fd96e5ae421ae77b
liuze713 <liuze713@hotmail.com>
2017-12-31 11:46:19 +0800

    监控指挥

--- monitor-server/src/test/java/com/rzt/MonitorServerApplicationTests.java
+++ monitor-server/src/test/java/com/rzt/MonitorServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rzt.khtask;
+package com.rzt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NursetaskServerApplicationTests {
+public class MonitorServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
