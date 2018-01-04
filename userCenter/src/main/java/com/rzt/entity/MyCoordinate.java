62ceb1dd6fabf961a1bc01ff6dd661c1d2ac4292
宁伟泽 <907695276@qq.com>
2018-01-05 00:23:50 +0800

    redis配置信息完善(增加连接池的其他相关信息)

---
+++ userCenter/src/main/java/com/rzt/entity/MyCoordinate.java
@@ -0,0 +1,187 @@
+package com.rzt.entity;
+
+public class MyCoordinate {
+
+    private Integer WORKTYPE;
+
+    private String WORKYEAR;
+
+    private String REALNAME;
+
+    private String CERTIFICATE;
+
+    private String PHONE;
+
+    private String USERDELETE;
+
+    private String EMAIL;
+
+    private String CLASSNAME;
+
+    private String USERTYPE;
+
+    private String USERNAME;
+
+    private String COMPANYNAME;
+
+    private String DEPT;
+
+    private String COMPANYID;
+
+    private String ID;
+
+    private String DEPTID;
+
+    private String ROLEID;
+
+    private Integer LOGINSTATUS;
+
+    private String AGE;
+
+    private String CLASSID;
+
+    private String imei2;
+
+    private float longitude;
+    private float latitude;
+
+
+    public String getImei2() {
+        return imei2;
+    }
+
+    public void setImei2(String imei2) {
+        this.imei2 = imei2;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setWORKTYPE(Integer WORKTYPE){
+        this.WORKTYPE = WORKTYPE;
+    }
+    public Integer getWORKTYPE(){
+        return this.WORKTYPE;
+    }
+    public void setWORKYEAR(String WORKYEAR){
+        this.WORKYEAR = WORKYEAR;
+    }
+    public String getWORKYEAR(){
+        return this.WORKYEAR;
+    }
+    public void setREALNAME(String REALNAME){
+        this.REALNAME = REALNAME;
+    }
+    public String getREALNAME(){
+        return this.REALNAME;
+    }
+    public void setCERTIFICATE(String CERTIFICATE){
+        this.CERTIFICATE = CERTIFICATE;
+    }
+    public String getCERTIFICATE(){
+        return this.CERTIFICATE;
+    }
+    public void setPHONE(String PHONE){
+        this.PHONE = PHONE;
+    }
+    public String getPHONE(){
+        return this.PHONE;
+    }
+    public void setUSERDELETE(String USERDELETE){
+        this.USERDELETE = USERDELETE;
+    }
+    public String getUSERDELETE(){
+        return this.USERDELETE;
+    }
+    public void setEMAIL(String EMAIL){
+        this.EMAIL = EMAIL;
+    }
+    public String getEMAIL(){
+        return this.EMAIL;
+    }
+    public void setCLASSNAME(String CLASSNAME){
+        this.CLASSNAME = CLASSNAME;
+    }
+    public String getCLASSNAME(){
+        return this.CLASSNAME;
+    }
+    public void setUSERTYPE(String USERTYPE){
+        this.USERTYPE = USERTYPE;
+    }
+    public String getUSERTYPE(){
+        return this.USERTYPE;
+    }
+    public void setUSERNAME(String USERNAME){
+        this.USERNAME = USERNAME;
+    }
+    public String getUSERNAME(){
+        return this.USERNAME;
+    }
+    public void setCOMPANYNAME(String COMPANYNAME){
+        this.COMPANYNAME = COMPANYNAME;
+    }
+    public String getCOMPANYNAME(){
+        return this.COMPANYNAME;
+    }
+    public void setDEPT(String DEPT){
+        this.DEPT = DEPT;
+    }
+    public String getDEPT(){
+        return this.DEPT;
+    }
+    public void setCOMPANYID(String COMPANYID){
+        this.COMPANYID = COMPANYID;
+    }
+    public String getCOMPANYID(){
+        return this.COMPANYID;
+    }
+    public void setID(String ID){
+        this.ID = ID;
+    }
+    public String getID(){
+        return this.ID;
+    }
+    public void setDEPTID(String DEPTID){
+        this.DEPTID = DEPTID;
+    }
+    public String getDEPTID(){
+        return this.DEPTID;
+    }
+    public void setROLEID(String ROLEID){
+        this.ROLEID = ROLEID;
+    }
+    public String getROLEID(){
+        return this.ROLEID;
+    }
+    public void setLOGINSTATUS(Integer LOGINSTATUS){
+        this.LOGINSTATUS = LOGINSTATUS;
+    }
+    public Integer getLOGINSTATUS(){
+        return this.LOGINSTATUS;
+    }
+    public void setAGE(String AGE){
+        this.AGE = AGE;
+    }
+    public String getAGE(){
+        return this.AGE;
+    }
+    public void setCLASSID(String CLASSID){
+        this.CLASSID = CLASSID;
+    }
+    public String getCLASSID(){
+        return this.CLASSID;
+    }
+}
