df5c7e1e3c2d842dfbfd0bb1d7d63f4ec3934dde
hyn_java <hyn_java@163.com>
2018-01-07 22:09:37 +0800

    t

--- monitor-server/src/main/java/com/rzt/service/XSZCTASKService.java
+++ monitor-server/src/main/java/com/rzt/service/XSZCTASKService.java
@@ -69,17 +69,17 @@ public class XSZCTASKService extends CurdService<TimedTask,XSZCTASKRepository>{
 
                 Map<String, Object> next = iterator.next();
                 Object taskid = next.get("TASKID");
-                Integer tasktype = (Integer) next.get("TASKTYPE");
+                Object tasktype = next.get("TASKTYPE");
                 //巡视
-                if(tasktype == 1){
-                    String sqll = "SELECT  c.LINE_ID FROM XS_ZC_TASK x LEFT JOIN XS_ZC_CYCLE c ON c.ID = x.XS_ZC_CYCLE_ID WHERE ID =?1";
+                if(tasktype!=null && tasktype.equals("1")){
+                    String sqll = "SELECT  c.LINE_ID FROM XS_ZC_TASK x LEFT JOIN XS_ZC_CYCLE c ON c.ID = x.XS_ZC_CYCLE_ID WHERE x.ID =?1";
                     List<Map<String, Object>> maps = execSql(sqll,taskid);
                     next.put("LINE_ID",maps.get(0).get("LINE_ID"));
-                }else if (tasktype==2){ //看护
+                }else if (tasktype!=null && tasktype.equals("2")){ //看护
                     String sqlll = "SELECT LINE_ID FROM KH_YH_HISTORY WHERE TASK_ID =1?";
                     List<Map<String, Object>> maps = execSql(sqlll, taskid);
                     next.put("LINE_ID",maps.get(0).get("LINE_ID"));
-                }else if (tasktype==3){ //稽查
+                }else if (tasktype!=null && tasktype.equals("3")){ //稽查
 
                 }
 
