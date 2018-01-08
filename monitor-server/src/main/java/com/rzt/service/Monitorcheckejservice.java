fdda093cd8c2739a9cb56295649f6729ff54ae65
hyn_java <hyn_java@163.com>
2018-01-08 18:10:31 +0800

    22

--- monitor-server/src/main/java/com/rzt/service/Monitorcheckejservice.java
+++ monitor-server/src/main/java/com/rzt/service/Monitorcheckejservice.java
@@ -7,8 +7,12 @@
 package com.rzt.service;
 import com.rzt.entity.Monitorcheckej;
 import com.rzt.repository.Monitorcheckejrepository;
+import com.rzt.utils.SnowflakeIdWorker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**      
  * 类名称：MONITORCHECKEJService    
@@ -21,8 +25,19 @@ import org.springframework.transaction.annotation.Transactional;
  * @version        
  */
 @Service
-@Transactional
 public class Monitorcheckejservice extends CurdService<Monitorcheckej,Monitorcheckejrepository> {
 
+    @Autowired
+    private Monitorcheckejrepository resp;
 
+
+    @Transactional
+    public void saveCheckEj(String[] messages) {
+        Monitorcheckej ej = new Monitorcheckej();
+        ej.setId(Long.valueOf(new SnowflakeIdWorker(0,0).nextId()));
+        ej.setCreateTime(new Date());
+        ej.setTaskType(messages[1]);
+        ej.setTaskId(messages[2]);
+        resp.save(ej);
+    }
 }
