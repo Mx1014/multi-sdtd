3dd9f0fe016a242b48d683e590b94f56fb02d124
hyn_java <hyn_java@163.com>
2018-01-08 16:20:02 +0800

    问题检查

---
+++ monitor-server/src/main/java/com/rzt/redisListener/DSRedis.java
@@ -0,0 +1,53 @@
+package com.rzt.redisListener;
+
+import com.rzt.utils.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by huyuening on 2018/1/5.
+ */
+@Component
+public class DSRedis {
+
+    @Autowired
+    JedisPool jedisPool;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @Scheduled(cron = "0/1 * * * * ? ")
+    public void addString(){
+        Jedis jedis = jedisPool.getResource();
+        try {
+
+           //jedis.setex("a",5,"123456");
+            jedis.select(1);
+            String date = "2019-01-08 15:05:00";
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date strtodate = formatter.parse(date);
+                jedis.psetex(String.valueOf(UUID.randomUUID()), Long.parseLong(String.valueOf(strtodate.getTime() - DateUtil.dateNow().getTime())) / 1000, "测试");
+            } catch (ParseException e) {
+                e.printStackTrace();
+                System.out.println("------");
+            }
+            System.out.println("dsfsdfsafsdfsadfdsfsfdsafssdf-------------------------===========================");
+
+        }catch (Exception e){
+            System.out.println("添加失败"+e.getMessage());
+        }finally {
+            jedis.close();
+        }
+    }
+
+}
