8b316f47ab617b742338df8caf530425eebce95d
hyn_java <hyn_java@163.com>
2018-01-09 16:57:30 +0800

    问题检查

--- monitor-server/src/main/java/com/rzt/repository/Monitorcheckyjrepository.java
+++ monitor-server/src/main/java/com/rzt/repository/Monitorcheckyjrepository.java
@@ -7,6 +7,7 @@
 package com.rzt.repository;
 import com.rzt.entity.Monitorcheckyj;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 /**
  * 类名称：MONITORCHECKYJRepository    
@@ -20,4 +21,7 @@ import org.springframework.stereotype.Repository;
  */
  @Repository
 public interface Monitorcheckyjrepository extends JpaRepository<Monitorcheckyj,String> {
+
+  @Query(value = "update MONITOR_CHECK_EJ set IS_VIEW = 1 where TASK_ID=?1 and TASK_TYPE = ?2 and WARNING_TYPE=?3",nativeQuery = true)
+  void changStatus(Long aLong, Integer integer, Integer integer1);
 }
