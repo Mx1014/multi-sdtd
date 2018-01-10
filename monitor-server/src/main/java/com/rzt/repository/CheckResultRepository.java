390e3868d785d9f3942fa1092380a6fe931be66d
liuze713 <liuze713@hotmail.com>
2018-01-10 13:13:03 +0800

    app流程

--- monitor-server/src/main/java/com/rzt/repository/CheckResultRepository.java
+++ monitor-server/src/main/java/com/rzt/repository/CheckResultRepository.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface CheckResultRepository extends JpaRepository<CheckResult, String> {
     @Modifying
-    @Query(value = " INSERT INTO MONITOR_CHECK_EJ (ID, TASK_ID, CREATE_TIME_Z, TASK_TYPE, STATUS, WARNING_TYPE,USER_ID,DEPTID,TASK_NAME) " +
+    @Query(value = " INSERT INTO MONITOR_CHECK_EJ (ID, TASK_ID, CREATE_TIME, TASK_TYPE, STATUS, WARNING_TYPE,USER_ID,DEPTID,TASK_NAME) " +
             "            VALUES (?1, ?2, sysdate, 2, 0, ?3,?4,?5,6) ", nativeQuery = true)
     void xsTourScope(Long id, Long taskid, Integer warningtype, String orgid, String userid, String taskname);
 
