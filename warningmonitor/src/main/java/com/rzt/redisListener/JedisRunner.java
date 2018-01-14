2608ec35cad4e7c7680fa68f146f2f010eaaa5e1
hyn_java <hyn_java@163.com>
2018-01-14 19:52:04 +0800

    rr

--- warningmonitor/src/main/java/com/rzt/redisListener/JedisRunner.java
+++ warningmonitor/src/main/java/com/rzt/redisListener/JedisRunner.java
@@ -1,7 +1,8 @@
 package com.rzt.redisListener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
@@ -12,7 +13,7 @@ import redis.clients.jedis.JedisPool;
  * Created by huyuening on 2018/1/5.
  */
 @Component
-public class JedisRunner implements CommandLineRunner {
+public class JedisRunner implements ApplicationListener<ContextRefreshedEvent>/* implements CommandLineRunner*/ {
 
     @Autowired
     JedisPool jedisPool;
@@ -24,6 +25,45 @@ public class JedisRunner implements CommandLineRunner {
     private Subscriber subscriber;
 
     @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        System.out.println("----------");
+        //监听所有reids通道中的过期事件
+        //subscribe();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Jedis jedis= jedisPool.getResource();
+                try {
+                    System.out.println("============");
+                    jedis.psubscribe(subscriber , "__keyevent@1__:expired");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    jedis.close();
+                }
+            }
+        });
+        t.start();
+    }
+//    @Async
+    private void subscribe() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Jedis jedis= jedisPool.getResource();
+                try {
+                    System.out.println("============");
+                    jedis.psubscribe(subscriber , "__keyevent@1__:expired");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    jedis.close();
+                }
+            }
+        });
+    }
+
+    /*@Override
     public void run(String... strings) throws Exception {
         Jedis jedis= jedisPool.getResource();
         try {
@@ -34,6 +74,6 @@ public class JedisRunner implements CommandLineRunner {
         }finally {
             jedis.close();
         }
-    }
+    }*/
 
 }
