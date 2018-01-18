19f92bcfeb7b9f72310e5f028d3dabe72118169c
闫恭轩 <ygx_job@126.com>
2018-01-18 18:17:22 +0800

    看护app填写未到位原因

--- nursetask-server/src/main/java/com/rzt/controller/KhTaskController.java
+++ nursetask-server/src/main/java/com/rzt/controller/KhTaskController.java
@@ -1,10 +1,11 @@
-/**    
+/**
  * 文件名：KhTaskController
- * 版本信息：    
- * 日期：2017/11/28 14:43:44    
- * Copyright 融智通科技(北京)股份有限公司 版权所有    
+ * 版本信息：
+ * 日期：2017/11/28 14:43:44
+ * Copyright 融智通科技(北京)股份有限公司 版权所有
  */
 package com.rzt.controller;
+
 import com.alibaba.fastjson.JSONObject;
 import com.rzt.entity.KhSite;
 import com.rzt.entity.KhTask;
@@ -32,126 +33,165 @@ import java.util.Set;
 @RestController
 @RequestMapping("KhTask")
 public class KhTaskController extends
-		CurdController<KhTask,KhTaskService> {
-	@Autowired
-	private KhYhHistoryService yhservice;
-	@Autowired
-	private RedisTemplate<String, Object> redisTemplate;
+        CurdController<KhTask, KhTaskService> {
+    @Autowired
+    private KhYhHistoryService yhservice;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
-	/***
-	 * 所有看护计划查询
-	 * @return
-	 */
-	@GetMapping("/listAllKhTask.do")
-	@ResponseBody
-	public WebApiResponse listAllKhTask(KhTaskModel task, String status,Pageable pageable) {
-		try {
-			//分页参数 page size
-			HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
-			JSONObject jsonObject = JSONObject.parseObject(hashOperations.get("UserInformation", task.getUserId()).toString());
-			Object o = this.service.listAllKhTask(task, status,pageable,Integer.valueOf(jsonObject.get("ROLETYPE").toString()));
-			//Object o = this.service.listAllKhTask(	task, status,pageable,0);
-			return WebApiResponse.success(o);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return WebApiResponse.erro("数据查询失败" + e.getMessage());
-		}
-	}
-	/**
-	 * 修改已安排任务
-	 */
-	@GetMapping("/updateTaskById.do")
-	@ResponseBody
-	public WebApiResponse updateTaskById(String khfzrId1,String id){
-	// 提交申请给 管理员  如何提交待定  还是说没有修改功能
-		try {
-			//分页参数 page size
- 				this.service.updateTaskById(khfzrId1,id);
-			return WebApiResponse.success("修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return WebApiResponse.erro("修改失败" + e.getMessage());
-		}
-	}
+    /***
+     * 所有看护计划查询
+     * @return
+     */
+    @GetMapping("/listAllKhTask.do")
+    @ResponseBody
+    public WebApiResponse listAllKhTask(KhTaskModel task, String status, Pageable pageable) {
+        try {
+            //分页参数 page size
+            HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+            JSONObject jsonObject = JSONObject.parseObject(hashOperations.get("UserInformation", task.getUserId()).toString());
+            Object o = this.service.listAllKhTask(task, status, pageable, Integer.valueOf(jsonObject.get("ROLETYPE").toString()));
+            //Object o = this.service.listAllKhTask(	task, status,pageable,0);
+            return WebApiResponse.success(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("数据查询失败" + e.getMessage());
+        }
+    }
 
-	/**
-	 * 查看已安排任务详情
-	 * @param id
-	 * @return
-	 */
-	@GetMapping("/getKhTaskById.do")
-	@ResponseBody
-	public WebApiResponse listKhTaskById(String id){
-		try {
-			List<Map<String, Object>> maps = this.service.getKhTaskById(Long.parseLong(id));
-			return WebApiResponse.success(maps);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return WebApiResponse.erro("数据获取失败" + e.getMessage());
-		}
-	}
+    /**
+     * 修改已安排任务
+     */
+    @GetMapping("/updateTaskById.do")
+    @ResponseBody
+    public WebApiResponse updateTaskById(String khfzrId1, String id) {
+        // 提交申请给 管理员  如何提交待定  还是说没有修改功能
+        try {
+            //分页参数 page size
+            this.service.updateTaskById(khfzrId1, id);
+            return WebApiResponse.success("修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("修改失败" + e.getMessage());
+        }
+    }
 
-	//任务查询页面的导出文件
-	@GetMapping("/exportKhTask.do")
-	public void exportKhTask(HttpServletRequest request, HttpServletResponse response,String userId){
-		try {
-			HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
-			JSONObject jsonObject = JSONObject.parseObject(hashOperations.get("UserInformation", userId).toString());
-			List<Map<String, Object>> taskList = this.service.findAlls(jsonObject,userId);
-			this.service.exportNursePlan(taskList,request,response);
-			//return WebApiResponse.success("");
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-	}
-	/**
-	 *   获取某人当前的看护任务
-	 */
-	@GetMapping("/listCurrentTaskByUserId")
-	public WebApiResponse listCurrentTaskByUserId(String userId,String startDate){
-		return this.service.listCurrentTaskByUserId(userId,startDate);
-	}
+    /**
+     * 查看已安排任务详情
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/getKhTaskById.do")
+    @ResponseBody
+    public WebApiResponse listKhTaskById(String id) {
+        try {
+            List<Map<String, Object>> maps = this.service.getKhTaskById(Long.parseLong(id));
+            return WebApiResponse.success(maps);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("数据获取失败" + e.getMessage());
+        }
+    }
 
-	/**
-	 * 隐患台账获取看护任务详情
-	 * @param yhId
-	 * @return
-	 */
-	@GetMapping("/listTaskInfoByYhId")
-	@ResponseBody
-	public WebApiResponse listTaskInfoByYhId(String yhId){
-		return this.service.listTaskInfoByYhId(yhId);
-	}
-	//隐患台账展示隐患图片
-	@GetMapping("/listPictureByYhId")
-	@ResponseBody
-	public WebApiResponse listPictureByYhId(String yhId){
-		return this.service.listPictureByYhId(yhId);
-	}
+    //任务查询页面的导出文件
+    @GetMapping("/exportKhTask.do")
+    public void exportKhTask(HttpServletRequest request, HttpServletResponse response, String userId) {
+        try {
+            HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+            JSONObject jsonObject = JSONObject.parseObject(hashOperations.get("UserInformation", userId).toString());
+            List<Map<String, Object>> taskList = this.service.findAlls(jsonObject, userId);
+            this.service.exportNursePlan(taskList, request, response);
+            //return WebApiResponse.success("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取某人当前的看护任务
+     */
+    @GetMapping("/listCurrentTaskByUserId")
+    public WebApiResponse listCurrentTaskByUserId(String userId, String startDate) {
+        return this.service.listCurrentTaskByUserId(userId, startDate);
+    }
+
+    /**
+     * 隐患台账获取看护任务详情
+     *
+     * @param yhId
+     * @return
+     */
+    @GetMapping("/listTaskInfoByYhId")
+    @ResponseBody
+    public WebApiResponse listTaskInfoByYhId(String yhId) {
+        return this.service.listTaskInfoByYhId(yhId);
+    }
+
+    //隐患台账展示隐患图片
+    @GetMapping("/listPictureByYhId")
+    @ResponseBody
+    public WebApiResponse listPictureByYhId(String yhId) {
+        return this.service.listPictureByYhId(yhId);
+    }
 
 
-	//地图展示某人的具体任务信息
-	@GetMapping("/listTaskInfoById")
-	@ResponseBody
-	public WebApiResponse listTaskInfoById(String taskId){
-		return this.service.listTaskInfoById(taskId);
-	}
+    //地图展示某人的具体任务信息
+    @GetMapping("/listTaskInfoById")
+    @ResponseBody
+    public WebApiResponse listTaskInfoById(String taskId) {
+        return this.service.listTaskInfoById(taskId);
+    }
 
-	//地图展示任务图片
-	@ApiOperation(value = "获取任务图片", notes = "获取任务图片  ")
-	@GetMapping("/appListPicture")
-	@ResponseBody
-	public WebApiResponse appListPicture(long taskId,Integer zj){
-		return this.service.appListPicture(taskId,zj);
-	}
+    //地图展示任务图片
+    @ApiOperation(value = "获取任务图片", notes = "获取任务图片  ")
+    @GetMapping("/appListPicture")
+    @ResponseBody
+    public WebApiResponse appListPicture(long taskId, Integer zj) {
+        return this.service.appListPicture(taskId, zj);
+    }
 
-	//定时任务
-	@GetMapping("/createTask")
-	@ResponseBody
-	public void createTask(String taskId){
-		this.service.createTask();
-	}
+    //定时任务
+    @GetMapping("/createTask")
+    @ResponseBody
+    public void createTask(String taskId) {
+        this.service.createTask();
+    }
 
+    //删除单、多个周期
+    @DeleteMapping("/deleteSiteById")
+    @ResponseBody
+    public WebApiResponse deleteSiteById(String id) {
+        try {
+            String[] split = id.split(",");
+            if (split.length > 0) {
+                for (int i = 0; i < split.length; i++) {
+                    this.service.deleteSiteById(Long.parseLong(split[i]));
+                }
+            }
+            return WebApiResponse.success("任务消缺成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("任务消缺失败" + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/deleteTaskById")
+    @ResponseBody
+    public WebApiResponse deleteTaskById(String id) {
+        try {
+            String[] split = id.split(",");
+            if (split.length > 0) {
+                for (int i = 0; i < split.length; i++) {
+                    this.service.deleteTaskById(Long.parseLong(split[i]));
+                }
+            }
+            return WebApiResponse.success("任务消缺成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WebApiResponse.erro("任务消缺失败" + e.getMessage());
+        }
+    }
 }
 
 
--- nursetask-server/src/main/java/com/rzt/repository/KhTaskRepository.java
+++ nursetask-server/src/main/java/com/rzt/repository/KhTaskRepository.java
@@ -1,10 +1,11 @@
-/**    
- * 文件名：KhTaskRepository           
- * 版本信息：    
- * 日期：2017/11/28 14:43:44    
- * Copyright 融智通科技(北京)股份有限公司 版权所有    
+/**
+ * 文件名：KhTaskRepository
+ * 版本信息：
+ * 日期：2017/11/28 14:43:44
+ * Copyright 融智通科技(北京)股份有限公司 版权所有
  */
 package com.rzt.repository;
+
 import com.rzt.entity.KhTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -16,37 +17,34 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Date;
 
 /**
- * 类名称：KhTaskRepository    
- * 类描述：    
- * 创建人：张虎成   
- * 创建时间：2017/11/28 14:43:44 
- * 修改人：张虎成    
- * 修改时间：2017/11/28 14:43:44    
- * 修改备注：    
- * @version        
+ * 类名称：KhTaskRepository
+ * 类描述：
+ * 创建人：张虎成
+ * 创建时间：2017/11/28 14:43:44
+ * 修改人：张虎成
+ * 修改时间：2017/11/28 14:43:44
+ * 修改备注：
  */
- @Repository
-public interface KhTaskRepository extends JpaRepository<KhTask,String> {
+@Repository
+public interface KhTaskRepository extends JpaRepository<KhTask, String> {
 
 
-
-    @Query(value = "SELECT COUNT(*) FROM kh_task where status != 3",nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM kh_task where status != 3", nativeQuery = true)
     int getcount();
 
-    @Query(value = "SELECT COUNT(*) FROM kh_task where site_id=?1 and user_id=?2",nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM kh_task where site_id=?1 and user_id=?2", nativeQuery = true)
     int getCount(long id, String userId);
 
 
-
     @Modifying
-    @Query(value = "update kh_task set wpqr_time = ?1 where id = ?2",nativeQuery = true)
+    @Query(value = "update kh_task set wpqr_time = ?1 where id = ?2", nativeQuery = true)
     void updateWPQRTime(Date time, long id);
 
     @Modifying
-    @Query(value = "update kh_task set real_start_time = ?1 where id = ?2",nativeQuery = true)
+    @Query(value = "update kh_task set real_start_time = ?1 where id = ?2", nativeQuery = true)
     void updateRealStartTime(Date time, long id);
 
-    @Query(value = "SELECT COUNT(*) FROM kh_task where status = 1",nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM kh_task where status = 1", nativeQuery = true)
     int getCount2();
 
   /*  @Modifying
@@ -56,24 +54,32 @@ public interface KhTaskRepository extends JpaRepository<KhTask,String> {
 
     @Modifying
     @Transactional
-    @Query(value = "update KH_SITE set user_id = ?1 where id = ?2",nativeQuery = true)
+    @Query(value = "update KH_SITE set user_id = ?1 where id = ?2", nativeQuery = true)
     void updateSiteById(String userId, String id);
 
     @Modifying
     @Transactional
-    @Query(value = "update KH_TASK set user_id = ?1 where SITE_ID = ?2 AND PLAN_START_TIME >=sysdate and status=0",nativeQuery = true)
+    @Query(value = "update KH_TASK set user_id = ?1 where SITE_ID = ?2 AND PLAN_START_TIME >=sysdate and status=0", nativeQuery = true)
     void updateTaskBySiteId(String userId, String id);
 
     @Modifying
     @Transactional
-    @Query(value = "update kh_site set PLAN_START_TIME = ?1,PLAN_END_TIME=?2,COUNT=?4 where id = ?3",nativeQuery = true)
+    @Query(value = "update kh_site set PLAN_START_TIME = ?1,PLAN_END_TIME=?2,COUNT=?4 where id = ?3", nativeQuery = true)
     void updateSite(Date startTime, Date endTime, Long id, int count);
 
     @Modifying
     @Transactional
-    @Query(value ="INSERT INTO KH_TASK (ID,SITE_ID,USER_ID,TASK_NAME,YH_ID,CREATE_TIME,STATUS,PLAN_START_TIME,PLAN_END_TIME,WX_ORG,COUNT,TDYW_ORG,ZXYS_NUM) " +
-            " VALUES (?1,?2,?3,?4,?5,sysdate,0,?6,?7,?8,?9,?10,0)",nativeQuery = true)
+    @Query(value = "INSERT INTO KH_TASK (ID,SITE_ID,USER_ID,TASK_NAME,YH_ID,CREATE_TIME,STATUS,PLAN_START_TIME,PLAN_END_TIME,WX_ORG,COUNT,TDYW_ORG,ZXYS_NUM) " +
+            " VALUES (?1,?2,?3,?4,?5,sysdate,0,?6,?7,?8,?9,?10,0)", nativeQuery = true)
     void addTask(Long id, Long siteId, String userId, String taskName, Long yhId, Date planStartTime, Date planEndTime, String wxOrg, int count, String tdywOrg);
 
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM KH_SITE where id=?1", nativeQuery = true)
+    void deleteSiteById(long id);
 
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM KH_TASK where id=?1", nativeQuery = true)
+    void deleteTaskById(long id);
 }
