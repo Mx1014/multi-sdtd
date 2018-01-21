6a731e32ad749a17353907d80a1517a80ef395a6
lichengyang <599303844@qq.com>
2018-01-21 14:37:15 +0800

    监听器注入问题解决

--- monitor-server/src/main/java/com/rzt/activiti/listener/ProListener.java
+++ monitor-server/src/main/java/com/rzt/activiti/listener/ProListener.java
@@ -1,13 +1,24 @@
 package com.rzt.activiti.listener;
 
+import com.rzt.activiti.service.impl.ProServiceImpl;
+import com.rzt.utils.SpringUtil;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 李成阳
  * 2018/1/17
  */
+@Component
 public class ProListener implements TaskListener {
+
 
 
     /**
@@ -16,7 +27,23 @@ public class ProListener implements TaskListener {
      */
     @Override
     public void notify(DelegateTask delegateTask) {
-        System.out.println("进入节点监听");
+        ProServiceImpl proServiceImpl = (ProServiceImpl) SpringUtil.getObject("proServiceImpl");
+
+        //查询当前流程是否由看护任务
+
+        String idByProId = proServiceImpl.findIdByProId(delegateTask.getProcessInstanceId());
+        String khid = (String) proServiceImpl.checkTask(idByProId, "khid");
+        //取消看护任务
+        if(null != khid && !"".equals(khid)){
+
+            System.out.println("取消看护任务"+khid);
+
+        }
+        System.out.println("进入节点监听"+khid);
+        //结束流程
+        proServiceImpl.complete(delegateTask.getId(),null);
         System.out.println(delegateTask);
     }
+
+
 }
