6dbef080e89178a51ce5927859cf9257cce1c5fd
李泽州 <lizezhou1231@outlook.com>
2018-01-22 19:08:28 +0800

    字符串拼接改为StringBuilder

--- file-server/src/main/java/com/rzt/service/PICTURETOURService.java
+++ file-server/src/main/java/com/rzt/service/PICTURETOURService.java
@@ -178,7 +178,7 @@ public class PICTURETOURService extends CurdService<PICTURETOUR,PICTURETOURRepos
 
     }
 
-    public Object getImgsByExecId(String execId) {
+    public Map<String, Object> getImgsByExecId(String execId) {
         Map<String, Object> result = new HashMap<>();
         ArrayList<String> params = new ArrayList<>();
         String sql= "select id,file_path,file_small_path,PROCESS_NAME from picture_tour where 1=1 ";
