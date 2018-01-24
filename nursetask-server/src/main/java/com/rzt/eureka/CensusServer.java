22199442535b6c6e9dc7f3d9d8fc9637c9274c48
闫恭轩 <ygx_job@126.com>
2018-01-24 16:35:22 +0800

    监控指挥推送一键告警

--- nursetask-server/src/main/java/com/rzt/eureka/CensusServer.java
+++ nursetask-server/src/main/java/com/rzt/eureka/CensusServer.java
@@ -3,12 +3,13 @@ package com.rzt.eureka;
 import com.rzt.util.WebApiResponse;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("CENSUSSERVER")
 public interface CensusServer {
 
-    @GetMapping("censusServer/WarningOneKey/warningKey\n")
+    @PostMapping("censusServer/WarningOneKey/warningKey\n")
     void warningKey(@RequestParam("id") Long  id);
 
 }
