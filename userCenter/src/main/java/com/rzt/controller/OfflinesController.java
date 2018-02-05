351bac2581454d2b9f7ea24ede2dfa6238bae1bb
hyn_java <hyn_java@163.com>
2018-02-05 11:16:24 +0800

    t

--- userCenter/src/main/java/com/rzt/controller/OfflinesController.java
+++ userCenter/src/main/java/com/rzt/controller/OfflinesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("OFFLINES")
@@ -57,58 +58,89 @@ public class OfflinesController extends CurdController<RztSysUser, CommonService
         if (!StringUtils.isEmpty(taskType)) {
             s += " and TASK_TYPE = " + taskType;
         }
-        /*String sql = " SELECT\n" +
-                "  e.USER_ID,\n" +
-                "  u.REALNAME,\n" +
-                "  u.CLASSNAME,\n" +
-                "  u.DEPT,\n" +
-                "  u.COMPANYNAME,\n" +
-                "  CASE u.WORKTYPE\n" +
-                "  WHEN 1\n" +
-                "    THEN '看护'\n" +
-                "  WHEN 2\n" +
-                "    THEN '巡视'\n" +
-                "  WHEN 3\n" +
-                "    THEN '现场稽查' END AS WORKTYPE,\n" +
-                "  e.a          AS MORE,\n" +
-                "  u.DEPTID,\n" +
-                "  e.CREATE_TIME,\n" +
-                "  e.ONLINE_TIME\n" +
-                "FROM (SELECT\n" +
-                "count(1) as a,\n" +
-                "USER_ID, MAX (CREATE_TIME) AS CREATE_TIME,\n" +
-                "nvl(to_char( MAX (ONLINE_TIME), 'yyyy-MM-dd hh24:mi:ss'), '人员未上线') as ONLINE_TIME\n" +
-                "FROM MONITOR_CHECK_EJ\n" +
-                "WHERE (WARNING_TYPE = 8 OR WARNING_TYPE = 2) " + s + " GROUP BY USER_ID) e LEFT JOIN USERINFO u\n" +
+        /*String sql = " SELECT " +
+                "  e.USER_ID, " +
+                "  u.REALNAME, " +
+                "  u.CLASSNAME, " +
+                "  u.DEPT, " +
+                "  u.COMPANYNAME, " +
+                "  CASE u.WORKTYPE " +
+                "  WHEN 1 " +
+                "    THEN '看护' " +
+                "  WHEN 2 " +
+                "    THEN '巡视' " +
+                "  WHEN 3 " +
+                "    THEN '现场稽查' END AS WORKTYPE, " +
+                "  e.a          AS MORE, " +
+                "  u.DEPTID, " +
+                "  e.CREATE_TIME, " +
+                "  e.ONLINE_TIME " +
+                "FROM (SELECT " +
+                "count(1) as a, " +
+                "USER_ID, MAX (CREATE_TIME) AS CREATE_TIME, " +
+                "nvl(to_char( MAX (ONLINE_TIME), 'yyyy-MM-dd hh24:mi:ss'), '人员未上线') as ONLINE_TIME " +
+                "FROM MONITOR_CHECK_EJ " +
+                "WHERE (WARNING_TYPE = 8 OR WARNING_TYPE = 2) " + s + " GROUP BY USER_ID) e LEFT JOIN USERINFO u " +
                 "ON e.USER_ID = u.ID ";*/
-        String sql = "SELECT DISTINCT ce.USER_ID,ce.REASON,ch.* FROM(\n" +
-                " SELECT\n" +
-                "   e.USER_ID,\n" +
-                "    u.REALNAME,\n" +
-                "    u.CLASSNAME,\n" +
-                "    u.DEPT,\n" +
-                "    u.COMPANYNAME,\n" +
-                "    CASE u.WORKTYPE\n" +
-                "    WHEN 1\n" +
-                "      THEN '看护'\n" +
-                "    WHEN 2\n" +
-                "      THEN '巡视'\n" +
-                "    WHEN 3\n" +
-                "      THEN '现场稽查' END AS WORKTYPE,\n" +
-                "    e.a          AS MORE,\n" +
-                "    u.DEPTID,\n" +
-                "    e.CREATE_TIME,\n" +
-                "    e.ONLINE_TIME\n" +
-                "     FROM (SELECT\n" +
-                "     count(1) as a,\n" +
-                "     USER_ID, MAX (CREATE_TIME) AS CREATE_TIME,\n" +
-                "     nvl(to_char( MAX (ONLINE_TIME), 'yyyy-MM-dd hh24:mi:ss'), '人员未上线') as ONLINE_TIME\n" +
-                "     FROM MONITOR_CHECK_EJ\n" +
-                "     WHERE (WARNING_TYPE = 8 OR WARNING_TYPE = 2)  " + s + "  GROUP BY USER_ID) e LEFT JOIN USERINFO u\n" +
-                "     ON e.USER_ID = u.ID) ch LEFT JOIN MONITOR_CHECK_EJ ce\n" +
-                "ON ch.USER_ID=ce.USER_ID AND ch.CREATE_TIME=ce.CREATE_TIME";
+        /*String sql = " SELECT DISTINCT ce.USER_ID,ce.REASON,ch.* FROM( " +
+                " SELECT " +
+                "   e.USER_ID, " +
+                "    u.REALNAME, " +
+                "    u.CLASSNAME, " +
+                "    u.DEPT, " +
+                "    u.COMPANYNAME, " +
+                "    CASE u.WORKTYPE " +
+                "    WHEN 1 " +
+                "      THEN '看护' " +
+                "    WHEN 2 " +
+                "      THEN '巡视' " +
+                "    WHEN 3 " +
+                "      THEN '现场稽查' END AS WORKTYPE, " +
+                "    e.a          AS MORE, " +
+                "    u.DEPTID, " +
+                "    e.CREATE_TIME, " +
+                "    e.ONLINE_TIME " +
+                "     FROM (SELECT " +
+                "     count(1) as a, " +
+                "     USER_ID, MAX (CREATE_TIME) AS CREATE_TIME, " +
+                "     nvl(to_char( MAX (ONLINE_TIME), 'yyyy-MM-dd hh24:mi:ss'), '人员未上线') as ONLINE_TIME " +
+                "     FROM MONITOR_CHECK_EJ " +
+                "     WHERE (WARNING_TYPE = 8 OR WARNING_TYPE = 2)  " + s + "  GROUP BY USER_ID) e LEFT JOIN USERINFO u " +
+                "     ON e.USER_ID = u.ID) ch LEFT JOIN MONITOR_CHECK_EJ ce " +
+                " ON ch.USER_ID=ce.USER_ID AND ch.CREATE_TIME=ce.CREATE_TIME ";*/
+        
+        String sql = "SELECT DISTINCT   " +
+                "  ce.USER_ID AS userID,   " +
+                "  ce.REASON,   " +
+                "  ch.*   " +
+                "FROM (SELECT   " +
+                "        e.USER_ID,   " +
+                "        u.REALNAME,   " +
+                "        u.CLASSNAME,   " +
+                "        u.DEPT,   " +
+                "        u.COMPANYNAME,   " +
+                "        CASE u.WORKTYPE   " +
+                "        WHEN 1   " +
+                "          THEN '看护'   " +
+                "        WHEN 2   " +
+                "          THEN '巡视'   " +
+                "        WHEN 3   " +
+                "          THEN '现场稽查' END AS WORKTYPE,   " +
+                "        e.a               AS MORE,   " +
+                "        u.DEPTID,   " +
+                "        e.CREATE_TIME,   " +
+                "        e.ONLINE_TIME   " +
+                "      FROM (SELECT   " +
+                "              count(1)    AS a,   " +
+                "              ej.USER_ID,   " +
+                "              MAX(ej.CREATE_TIME)       AS CREATE_TIME,   " +
+                "              nvl(to_char(MAX(ej.ONLINE_TIME), 'yyyy-MM-dd hh24:mi:ss'), '人员未上线') AS ONLINE_TIME   " +
+                "            FROM MONITOR_CHECK_EJ ej   " +
+                "            WHERE (ej.WARNING_TYPE = 8 OR ej.WARNING_TYPE = 2) " + s + 
+                "            GROUP BY USER_ID) e LEFT JOIN USERINFO u ON e.USER_ID = u.ID) ch LEFT JOIN MONITOR_CHECK_EJ ce   " +
+                "    ON ch.USER_ID = ce.USER_ID AND ch.CREATE_TIME = ce.CREATE_TIME";
         try {
-            return WebApiResponse.success(this.service.execSqlPage(pageable, sql, listLike.toArray()));
+            return WebApiResponse.success(this.service.execSqlPage(pageable,sql,listLike.toArray()));
         } catch (Exception e) {
             e.printStackTrace();
             return WebApiResponse.erro("");
