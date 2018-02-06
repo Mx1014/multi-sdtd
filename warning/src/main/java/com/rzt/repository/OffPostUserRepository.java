2d7672eb8d340e6d66bac34530af20fe5ab65449
hyn_java <hyn_java@163.com>
2018-02-06 18:23:30 +0800

    添加脱岗30分钟

--- warning/src/main/java/com/rzt/repository/OffPostUserRepository.java
+++ warning/src/main/java/com/rzt/repository/OffPostUserRepository.java
@@ -8,9 +8,12 @@ package com.rzt.repository;
 
 import com.rzt.entity.OffPostUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-/**      
+import org.springframework.transaction.annotation.Transactional;
+
+/**
  * 类名称：WarningOffPostUserRepository    
  * 类描述：    
  * 创建人：张虎成   
@@ -25,4 +28,8 @@ public interface OffPostUserRepository extends JpaRepository<OffPostUser,String>
  @Query(value = "select * from warning_off_post_user where user_id=?1 and task_id=?2",nativeQuery = true)
  OffPostUser findByUserIdAndTaskId(String userId, Long taskId);
 
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE WARNING_OFF_POST_USER_TIME SET TIME_STATUS=1 WHERE FK_TASK_ID=?1 AND FK_USER_ID=?2")
+    int updateTimeStatus(Object fk_task_id, Object fk_user_id);
 }
