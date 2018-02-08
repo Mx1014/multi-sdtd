791abd6a3b1c2d96bb0cd31f7a5c47a2cd53ad54
闫恭轩 <ygx_job@126.com>
2018-02-08 16:47:55 +0800

    定时删除人员已删除的看护周期

--- userCenter/src/main/java/com/rzt/utils/DateUtil.java
+++ userCenter/src/main/java/com/rzt/utils/DateUtil.java
@@ -10,6 +10,7 @@ import org.joda.time.DateTime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /***
@@ -32,6 +33,38 @@ public class DateUtil {
         partterns[0] = "yyyy-MM-dd HH:mm:ss";
     }
 
+    public static final Date getNowDate() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String cdate = sdf.format(cal.getTime());
+        return cal.getTime();
+    }
+
+    public static Date addDate(Date date, double hour) {
+        Calendar cal = Calendar.getInstance();
+        long hour1 = (long) (hour * 3600000);
+        long l = date.getTime() + hour1;
+        cal.setTimeInMillis(l);
+        date = cal.getTime();
+        // return format.format(date);
+        return date;
+    }
+
+    public static String timeUtil(int i) {
+        String date = "";
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        Date m = c.getTime();
+        String mon = df.format(m);
+        if (i == 1) {
+            date = mon + " 00:00";
+        } else {
+            date = mon + " 23:59";
+        }
+        //  task.setPlanEndTime(df.format(new Date()) + " 23:59");
+        return date;
+    }
 
     /**
      * @author LYTG
@@ -64,8 +97,10 @@ public class DateUtil {
     public static String stringNow() {
         return SDF.format(new Date());
     }
+
     /**
      * 获取当前日期
+     *
      * @return
      */
     public static String getCurrentDate() {
@@ -76,6 +111,7 @@ public class DateUtil {
 
     /**
      * 获取到天
+     *
      * @return
      */
     public static String dateFormatToDay(Date date) {
