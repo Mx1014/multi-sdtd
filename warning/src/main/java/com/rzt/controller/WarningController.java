e04e3620f499c04c4d93df65875b8256346d4a24
hyn_java <hyn_java@163.com>
2018-03-02 14:58:19 +0800

    tj

--- warning/src/main/java/com/rzt/controller/WarningController.java
+++ warning/src/main/java/com/rzt/controller/WarningController.java
@@ -49,33 +49,30 @@ public class WarningController extends CurdController<OffPostUser,WarningOffPost
 						service.addUser(offPostUser);
 						//添加OffPostUserTime
 						timeService.addOffUserTime(offPostUser.getUserId(),offPostUser.getTaskId());
-						/*try {
-							staffLine.khtg(offPostUser.getUserId(),offPostUser.getTaskId());
-						} catch (Exception e) {
-						}*/
+
 					}
 				}else{
 					//判断该人员状态是否改变
 					if (offUser.getStatus()!=offPostUser.getStatus()){
-						List<OffPostUserTime> list = timeService.findByUserIdAndDateisNull(offUser.getUserId(),offUser.getTaskId());
-						if(list.size()>0){
-							OffPostUserTime offPostUserTime = list.get(0);
-							//如果该条时间记录已经存在，则只更新回岗时间
-							//offPostUserTime.setEndTime(new Date());
-							timeService.updateOffUserEndTime(offPostUserTime);
-						}else{
-							//如果该条时间记录不存在，则重新添加一条
+						if(offPostUser.getStatus()==0){
+							//回岗
+							List<OffPostUserTime> list = timeService.findByUserIdAndDateisNull(offUser.getUserId(),offUser.getTaskId());
+
+							if(list.size()>0){
+								OffPostUserTime offPostUserTime = list.get(0);
+								//如果该条时间记录已经存在，则只更新回岗时间
+								//offPostUserTime.setEndTime(new Date());
+								timeService.updateOffUserEndTime(offPostUserTime);
+							}
+						}else if(offPostUser.getStatus()==1){
+							//脱岗
 							timeService.addOffUserTime(offPostUser.getUserId(),offPostUser.getTaskId());
 						}
+
 						//更新脱岗人员状态
 						offUser.setStatus(offPostUser.getStatus());
 						service.updateOffUser(offUser);
-						/*if(offPostUser.getStatus()==1){
-							try {
-								staffLine.khtg(offPostUser.getUserId(),offPostUser.getTaskId());
-							} catch (Exception e) {
-							}
-						}*/
+
 					}
 				}
 
