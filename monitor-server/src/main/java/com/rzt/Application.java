b803e993cb497e3ea6513d58ac41dfac5700310f
lichengyang <599303844@qq.com>
2018-03-12 11:40:49 +0800

    去掉一些不必要的注解

--- monitor-server/src/main/java/com/rzt/Application.java
+++ monitor-server/src/main/java/com/rzt/Application.java
@@ -4,15 +4,19 @@ package com.rzt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by huyuening on 2018/1/5.
  */
+@ServletComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan
 @EnableEurekaClient
 @EnableFeignClients
 @EnableScheduling
